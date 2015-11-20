1981716d52599ac911e03cfb32e0656022ed7cb0
Divyaganapathy <gdivya100@gmail.com>
2015-11-20 13:43:39 +0530

    Todos and todo
    Added controls and form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,13 +3,53 @@ angular.module('controllers', [])
 .controller('HomeCtrl', [
   '$scope',
   function($scope) {
-    
+
   },
   ])
 .controller('AccountCtrl', [
   '$scope',
   function($scope) {
-      
+
   },
   ]);
-    
+  .controller('TodosCtrl', function($scope, todos) {
+    $scope.todos = todos
+  })
+
+  .controller('TodoCtrl', function($scope, todo) {
+    $scope.todo = todo
+  })
+  
+  .service('TodosService', function($q) {
+    return {
+      todos: [
+        {
+          id: '1',
+          name: 'Read the book',
+          done: false
+        },
+        {
+          id: '2',
+          name: 'Wash the car',
+          done: false
+        },
+        {
+          id: '3',
+          name: 'Buy the grocery items',
+          done: true
+        }
+      ],
+      getTodos: function() {
+        return this.todos
+      },
+      getTodo: function(todoId) {
+        var dfd = $q.defer()
+        this.todos.forEach(function(todo) {
+          if (todo.id === todoId) dfd.resolve(todo)
+        })
+
+        return dfd.promise
+      }
+
+    }
+  });
